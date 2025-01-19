ed18c3747fdfe19b9245042f45f7fa0e2c1e8abb
joser27 <joser27@uw.edu>
2025-01-18 19:27:03 -0800

    fixed animtor bug and scale option.

--- animator.js
+++ animator.js
@@ -1,31 +1,28 @@
 class Animator {
-    constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration) {
-        Object.assign(this, {spritesheet, xStart, yStart, width, height, frameCount, frameDuration});
+	constructor(spritesheet, xStart, yStart, width, height, frameCount, frameDuration, scale = 1) {
+		Object.assign(this, { spritesheet, xStart, yStart, width, height, frameCount, frameDuration, scale });
 
-        this.elapsedTime = 0;
-        this.totalTime = frameCount * frameDuration;
-    };
+		this.elapsedTime = 0;
+		this.totalTime = frameCount * frameDuration;
+	};
 
-    drawFrame(tick, ctx, x, y) {
-        this.elapsedTime += tick;
+	drawFrame(tick, ctx, x, y) {
+		this.elapsedTime += tick;
+		if(this.elapsedTime > this.totalTime) this.elapsedTime %= this.totalTime;
+		const frame = this.currentFrame();
 
-        if (this.elapsedTime > this.totalTime) {
-            this.elapsedTime -= this.totalTime;
-        }
-        const frame = this.currentFrame();
+		ctx.drawImage(this.spritesheet,
+			this.xStart + this.width*frame, this.yStart,
+			this.width, this.height,
+			x, y,
+			this.width * this.scale, this.height * this.scale);
+	};
 
-        ctx.drawImage(this.spritesheet,
-            this.xStart + 5*(frame) + this.width*(frame), this.yStart,
-            this.width, this.height,
-            x, y,
-            this.width*2, this.height*2);
-    };
+	currentFrame(){
+		return Math.floor(this.elapsedTime / this.frameDuration);
+	};
 
-    currentFrame() {
-        return Math.floor(this.elapsedTime / this.frameDuration);
-    };
-
-    isDone() {
-        return (this.elapsedTime >= this.totalTime);
-    };
+	isDone() {
+		return (this.elapsedTime >= this.totalTime);
+	};
 }
