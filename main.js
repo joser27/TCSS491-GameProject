6d55933ce116ceed1f8aab0acb1b546548732aba
vishen805 <125772792+vishen805@users.noreply.github.com>
2025-01-21 21:34:37 -0800

    Revert "Add moving background(undone need character to move in the game to finish the speed of background move)"
    This reverts commit a3523dc8581135b555921e831e5c682cedbbabe4.

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
 const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
-ASSET_MANAGER.queueDownload("./background.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
 	const ctx = canvas.getContext("2d");
-
-	gameEngine.addEntity(new Background(gameEngine));
 
 	gameEngine.init(ctx);
 
